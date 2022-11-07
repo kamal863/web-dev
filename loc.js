d6402d4c3aa2ed8d0e07f6c97af7b8be87cc229b
kamal868 <kamalprajapat868@gmail.com>
2022-11-07 21:53:41 +0530

    message

---
+++ loc.js
@@ -0,0 +1,13 @@
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  const outerWear="sweater";
+  // Only change code below this line
+
+  // Only change code above this line
+  return outerWear;
+}
+
+const ans=myOutfit();
+console.log(ans);
